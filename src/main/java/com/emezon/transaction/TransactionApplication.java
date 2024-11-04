3b61440fcd9f5e782e7310f9b4e0ad06fd5eb69c
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-04 15:55:28 -0500

    first commit

---
+++ src/main/java/com/emezon/transaction/TransactionApplication.java
@@ -0,0 +1,13 @@
+package com.emezon.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionApplication.class, args);
+	}
+
+}
