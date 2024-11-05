816afc526e38acfec68da32556f3a7297992a2b8
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-05 00:12:33 -0500

    Added SupplyEntity

---
+++ src/main/java/com/emezon/transaction/infra/outbound/mysql/jpa/entities/SupplyEntity.java
@@ -0,0 +1,49 @@
+package com.emezon.transaction.infra.outbound.mysql.jpa.entities;
+
+import com.emezon.transaction.infra.outbound.mysql.jpa.constants.SupplyEntityConstants;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+
+@Entity(name = SupplyEntityConstants.ENTITY_NAME)
+@Table(name = SupplyEntityConstants.TABLE_NAME)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupplyEntity {
+
+    @Id
+    @UuidGenerator
+    private String id;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private String articleId;
+
+    @Column(nullable = false)
+    private String suppliedById;
+
+    private String comments;
+
+    private Double cost;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
