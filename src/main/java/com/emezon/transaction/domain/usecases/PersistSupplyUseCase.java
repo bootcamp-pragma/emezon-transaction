2606d43f0dc83a0ba9062ecbfd63159bacb1ce60
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-05 01:22:26 -0500

    Created PersistSupplyUseCase

---
+++ src/main/java/com/emezon/transaction/domain/usecases/PersistSupplyUseCase.java
@@ -0,0 +1,61 @@
+package com.emezon.transaction.domain.usecases;
+
+import com.emezon.transaction.domain.api.IPersistSupplyInPort;
+import com.emezon.transaction.domain.constants.SupplyConstrains;
+import com.emezon.transaction.domain.constants.SupplyErrorMessages;
+import com.emezon.transaction.domain.models.Supply;
+import com.emezon.transaction.domain.spi.IJwtService;
+import com.emezon.transaction.domain.spi.ISupplyRepositoryOutPort;
+
+public class PersistSupplyUseCase implements IPersistSupplyInPort {
+
+    private final ISupplyRepositoryOutPort repository;
+    private final IJwtService jwtService;
+
+    public PersistSupplyUseCase(ISupplyRepositoryOutPort repository, IJwtService jwtService) {
+        this.repository = repository;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Supply createSupply(Supply supply) {
+        Supply processedSupply = processAndValidateSupply(supply);
+        Supply newSupply = repository.save(processedSupply);
+        // llamar al microservicio de inventario para actualizar el stock
+        return newSupply;
+    }
+
+    @Override
+    public void deleteSupply(String id) {
+        repository.deleteById(id);
+    }
+
+    private Supply processAndValidateSupply(Supply supply) {
+        Supply.SupplyBuilder supplyBuilder = Supply.builder();
+        if (supply.getArticleId() == null || supply.getArticleId().trim().isEmpty()) {
+            throw new IllegalArgumentException(SupplyErrorMessages.SUPPLY_ARTICLE_ID_REQUIRED);
+        }
+        supplyBuilder.articleId(supply.getArticleId().trim());
+        if (supply.getSuppliedById() == null || supply.getSuppliedById().trim().isEmpty()) {
+            throw new IllegalArgumentException(SupplyErrorMessages.SUPPLY_SUPPLIED_BY_ID_REQUIRED);
+        }
+        supplyBuilder.suppliedById(supply.getSuppliedById().trim());
+        if (supply.getQuantity() < SupplyConstrains.SUPPLY_QUANTITY_MIN_VALUE) {
+            throw new IllegalArgumentException(SupplyErrorMessages.SUPPLY_QUANTITY_MIN_VALUE);
+        }
+        supplyBuilder.quantity(supply.getQuantity());
+        if (supply.getCost() != null && supply.getCost() < SupplyConstrains.SUPPLY_COST_MIN_VALUE) {
+            throw new IllegalArgumentException(SupplyErrorMessages.SUPPLY_COST_MIN_VALUE);
+        }
+        supplyBuilder.cost(supply.getCost());
+        if (supply.getComments() != null) {
+            if (supply.getComments().trim().length() > SupplyConstrains.SUPPLY_COMMENT_MAX_LENGTH) {
+                throw new IllegalArgumentException(SupplyErrorMessages.SUPPLY_COMMENTS_MAX_LENGTH);
+            }
+            supplyBuilder.comments(supply.getComments().trim());
+        }
+        supplyBuilder.restockDate(supply.getRestockDate());
+        return supplyBuilder.build();
+    }
+
+}
